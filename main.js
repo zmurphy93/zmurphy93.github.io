8b4d6f5e0854f660641f45d131af1dfa6df11ba0
zmurphy93 <zmurphy93@gmail.com>
2019-06-30 18:50:13 -0400

    reorganized it into a one page format.

--- main.js
+++ main.js
@@ -12,4 +12,17 @@ $(document).ready(function() {
 
     e.preventDefault();
   });
+
+  $(".flipper").click(function() {
+    var target = $( event.target );
+    if ( target.is("a") ) {
+      //follow that link
+      return true;
+    } else {
+      $(this).toggleClass("flip");
+    }
+    return false;
+  });
+
+
 });
